0ed0c533c6315e6a008cbde1cf86a37d9f3179eb
Devin Ledwell <devinledwell@gmail.com>
2023-11-27 23:00:24 -0330

    buildLambda script

---
+++ scripts/buildLambdas.ts
@@ -0,0 +1,26 @@
+import * as esbuild from "esbuild";
+import * as fs from "fs";
+/**
+ * TODO
+ * 1. Read in all the files from './src/lambdas' automatically (look at fs)
+ * 2. Change outdir to 'dist'
+ * 3. Verify that we are using the correct tsconfig
+ * 4. Replace build:src with the content from build:lambdas and then remove build:lambdas
+ * 5. Commit this all
+ */
+const lambdaDirectory = "./src/lambdas";
+const lambdaFiles = fs
+  .readdirSync(lambdaDirectory)
+  .filter((file) => file.endsWith(".ts"))
+  .map((file) => `./${lambdaDirectory}/${file}`);
+
+async function build() {
+  await esbuild.build({
+    entryPoints: lambdaFiles,
+    bundle: true,
+    outdir: "dist",
+    tsconfig: "./tsconfig.src.json",
+  });
+}
+
+build().then(() => "Done building");
