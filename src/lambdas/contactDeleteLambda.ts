829857260e278d9bc5db2e9beed49cbf2ddd5d93
Devin Ledwell <devinledwell@gmail.com>
2023-12-03 23:42:47 -0330

    Create contactDeleteLambda.ts

---
+++ src/lambdas/contactDeleteLambda.ts
@@ -0,0 +1,84 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+
+const dynamoDBClient = new DynamoDBClient({ region: "us-east-1" });
+const documentClient = DynamoDBDocumentClient.from(dynamoDBClient);
+const tableName = "Contacts";
+
+const deleteItem = async (itemId: string) => {
+  const params = {
+    TableName: tableName,
+    Key: {
+      ContactId: itemId,
+    },
+  };
+
+  try {
+    const command = new DeleteCommand(params);
+    await documentClient.send(command);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export async function deleteContactHandler(
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> {
+  const response = {
+    statusCode: 500,
+    body: "",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET,DELETE,PUT",
+    },
+  };
+
+  const itemId = event.pathParameters?.ContactId;
+
+  if (!itemId) {
+    const res = {
+      ...response,
+      statusCode: 400,
+      body: JSON.stringify({ error: "Invalid request. ContactId is missing." }),
+    };
+
+    console.log(JSON.stringify(res, null, 2));
+
+    return res;
+  }
+
+  try {
+    console.log(`Deleting contact with id: ${itemId}`);
+
+    await deleteItem(itemId);
+
+    const res = {
+      ...response,
+      statusCode: 204, // No content for a successful deletion
+    };
+
+    console.log(JSON.stringify(res, null, 2));
+
+    return res;
+  } catch (error) {
+    console.error("Error deleting item:", error);
+
+    const res = {
+      ...response,
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to delete item." }),
+    };
+
+    console.log(JSON.stringify(res, null, 2));
+
+    return res;
+  }
+}
